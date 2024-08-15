b71182a880e5ba2f995daf8b4cb2200c55b3a548
Frank Dev <frank.software.developer@gmail.com>
2024-08-15 09:47:11 +0200

    Database connection and quering working. DB connection setup as a mutex shared via a tauri shared state, so that multiple tauri command functions can access the connection. Feedback is only in the console atm.
    ToDo: need to implement disconnect, and all the feed back to the frontend.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,16 +3,17 @@
 
 use std::env;
 use std::{thread, time};
-use tauri::Window;
+use tauri::{Window, State};
+use std::sync::Mutex;
 use serde::Deserialize;
 use std::fs::{self, File};
 use std::io::prelude::*;
 use std::path::{Path};
 
-mod oracle_db;
 
+mod oracle_db;
 use oracle_db::{connect_to_oracle, execute_query, display_result_set};
-// use oracle::Result;
+use oracle::{Connection}; // Do not use oracle::Result here. It will messup std::Result;
 
 
 #[derive(Deserialize, Debug)]
@@ -33,9 +34,16 @@ struct SQLPayload {
     sql: String,
 }
 
-/// Converts an `oracle::Error` to a `String`
-fn error_to_string(error: oracle::Error) -> String {
-    error.to_string()
+pub struct DbConnection {
+    pub connection: Mutex<Option<Connection>>,
+}
+
+impl DbConnection {
+    pub fn new() -> Self {
+        DbConnection {
+            connection: Mutex::new(None),
+        }
+    }
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -68,50 +76,99 @@ fn save_content(payload: ContentPayload) -> Result<String, String> {
 }
 
 #[tauri::command]
-fn db_connect(payload: ConnectionPayload) -> Result<String, String> {
-    println!("db_connect(): ConnectionPayload '{:?}'", payload);
+fn db_connect(state: State<DbConnection>, payload: ConnectionPayload) -> Result<String, String> {
+    println!("db_connect(): '{:?}'", payload);
     
-    let connection_result: Result<oracle::Connection, oracle::Error> = connect_to_oracle(payload.username.as_str(), payload.password.as_str(), payload.database.as_str());
-    match connection_result {
-        Ok(connection) => {
-            println!("Connection succeeded.");
-            let version_result: Result<(oracle::Version, String), oracle::Error> = connection.server_version();
-            match version_result {
-                Ok((server_ver,banner)) => {
-                    // println!("\nDatabase Server Version: {}", version);
-                    println!("\nDatabase Server Version: {}", server_ver);
-                    println!("\nServer Banner: {}\n", banner);
-                },
-                Err(e) => {
-                    // Use the custom conversion function
-                    // let error_string = error_to_string(e);
-                    println!("Operation failed: {}", e);
-                }
-            }
-        },
-        Err(e) => {
-            // Use the custom conversion function
-            let error_string = error_to_string(e);
-            println!("Operation failed: {}", error_string);
-        }
-    }
+
+    let conn = Connection::connect(
+                            payload.username.as_str(), 
+                            payload.password.as_str(), 
+                            payload.database.as_str()
+                           ).map_err(|e| format!("Failed to connect: {}", e))?;
+    
+    // Lock the state and update the connection
+    let mut connection = state.connection.lock().unwrap();
+    *connection = Some(conn);
 
 
 
-    // let conn = connect_to_oracle(payload.username.as_str(), payload.password.as_str(), payload.database.as_str())?;
-    // let (server_ver, banner) = conn.server_version()?;
+    // let connection_result: Result<oracle::Connection, oracle::Error> = connect_to_oracle(payload.username.as_str(), payload.password.as_str(), payload.database.as_str());
 
-    // println!("\nDatabase Server Version: {}", server_ver);
-    // println!("\nServer Banner: {}\n", banner);
+    // match connection_result {
+    //     Ok(my_connection) => {
+    //         println!("Connection to DB succeeded.");
 
-    // Ok(format!("\nDatabase Server Version: {}\nServer Banner: {}\n", server_ver, banner))
+    //         // Get db version
+    //         let version_result: Result<(oracle::Version, String), oracle::Error> = my_connection.server_version();
+    //         match version_result {
+    //             Ok((server_ver,banner)) => {
+    //                 // println!("\nDatabase Server Version: {}", version);
+    //                 println!("\nDatabase Server Version: {}", server_ver);
+    //                 println!("\nServer Banner: {}\n", banner);
+    //             },
+    //             Err(e) => {
+    //                 println!("Connection to DB failed: {}", e);
+    //             }
+    //         }
+    //     },
+    //     Err(e) => {
+    //         // Use the custom conversion function to convert an oracle::Error to a String
+    //         // let error_string = error_to_string(e);
+    //         // println!("Operation failed: {}", error_string);
+    //         println!("Connection to DB failed: {}", e.to_string());
+    //     }
+    // }
 
     Ok(format!("db_connect {}", "done"))
 }
 
+// #[tauri::command]
+// fn db_disconnect(payload: ConnectionPayload) -> Result<String, String> {
+//     println!("db_disconnect(): '{:?}'", payload);
+    
+//     match DB_CONNECTION {
+//         Ok(connection) => {
+//             println!("Attempting disconnect ...");
+//             let close: Result<(), oracle::Error> = connection.close();
+//             match close {
+//                 Ok(()) => {
+//                     println!("Disconnection succeeded.");
+//                     println!("\nBye");
+//                 },
+//                 Err(e) => {
+//                     println!("Disconnect failed: {}", e);
+//                 }
+//             }
+//         },
+//         Err(e) => {
+//             println!("Disconnect failed: {}", e.to_string());
+//         }
+//     }
+
+//     Ok(format!("db_disconnect {}", "done"))
+// }
+
 #[tauri::command]
-fn db_query(payload: SQLPayload) -> Result<String, String> {
-    println!("db_query(): SQLPayload '{:?}'", payload);
+fn db_query(state: State<DbConnection>, payload: SQLPayload) -> Result<String, String> {
+    println!("db_query(): '{:?}'", payload);
+
+    let mut connection = state.connection.lock().unwrap();
+    
+    // If a connection exists, close it
+    if let Some(conn) = connection.take() {
+        println!("Connection exists. execute_query...");
+        
+        // let sql = "select count(asg_id) cnt from aansluitingen";
+        let (column_names, rows) = execute_query(&conn, payload.sql.as_str())
+                                                            .map_err(|e| format!("Query failed: {}", e))?;
+    
+        // Print the column names
+        println!("{:?}", column_names.join(", "));
+    
+        // Display the result set
+        display_result_set(rows);
+    }
+
     Ok(format!("db_query {}", "done"))
 }
 
@@ -120,6 +177,7 @@ fn main() {
     println!("Current directory: {}", current_dir.display());
 
     tauri::Builder::default()
+        .manage(DbConnection::new()) // Initialize the shared state
         .invoke_handler(tauri::generate_handler![
             do_with_progress,
             save_content,
--- backend/src/oracle_db.rs
+++ backend/src/oracle_db.rs
@@ -1,29 +1,52 @@
-use oracle::{Connection, Result, Row};
+use oracle::{Connection, Result, Row, ResultSet};
 
 /// Connects to an Oracle database and returns a `Connection` object.
 pub fn connect_to_oracle(username: &str, password: &str, service: &str) -> Result<Connection> {
+    println!("connect_to_oracle: ({:?}, {:?}, {:?})", username, password, service);
     let conn = Connection::connect(username, password, service)?;
     Ok(conn)
 }
 
 /// Executes an SQL query on the provided `Connection` and returns the column names and the result set for further use.
 pub fn execute_query<'a>(conn: &'a Connection, sql: &'a str) -> Result<(Vec<String>, Vec<Row>)> {
+    println!("execute_query: {:?}", sql);
+
+    let rows: ResultSet<'_,Row>;
+    let mut column_names: Vec<String> = Vec::new();
+    let mut result_rows: Vec<Row> = Vec::new();
+
+    println!("building statement ...");
     let mut stmt = conn.statement(sql).build()?;
-    let rows = stmt.query(&[])?;
+    println!("statement: {:?}", stmt);
+    println!("fetching rows ...");
+    // let rows = stmt.query(&[])?;
+    // println!("rows: {:?}", rows);
     
-    let column_names = rows.column_info().iter().map(|info| info.name().to_string()).collect();
+    rows = match stmt.query(&[]) {
+        Ok(resultset) => resultset,
+        Err(e) => {
+            println!("Failed to fetch rows: {}", e);
+            return Ok((column_names, result_rows))
+        }
+    };
+    println!("rows: {:?}", rows);
+
+    println!("fetching column_names ..");
+    column_names = rows.column_info().iter().map(|info| info.name().to_string()).collect();
     
-    let mut result_rows = Vec::new();
+    println!("fetching result_rows ..");
     for row_result in rows {
         let row = row_result?;
         result_rows.push(row);
     }
 
+    println!("returning (column_names, result_rows)");
     Ok((column_names, result_rows))
 }
 
 /// Displays the result set from a `Vec<Row>`.
 pub fn display_result_set(rows: Vec<Row>) {
+    println!("display_result_set:");
     for row in rows {
         for (idx, val) in row.sql_values().iter().enumerate() {
             if idx != 0 {
